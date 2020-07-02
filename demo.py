4d22ab7fc853a11fc4525889ebffe4bba4f0153e
ppulivarthi <ppulivarthi@dxc.com>
2020-07-02 14:04:35 +0530

    import code added

--- demo.py
+++ demo.py
@@ -1,2 +1,14 @@
-print("hello world")
-print("This is test file")
+#importing CSV,XLS and Tab files
+import pandas as pd
+def import_files(filename,extension):
+    if extension=="csv":
+        df=pd.read_csv(filename)
+    elif extension=="xls":
+        df=pd.read_excel(filename)
+    elif extension=="tab":
+        df=pd.read_csv(filename,sep='\t')
+    print("----Top 5 Rows----",df.head(),
+      "\n","-----Botom 5 Rows----",df.tail(),
+     "\n","-----columns names----",df.columns)
+    return df
+
